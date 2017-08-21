5edc5cd02222dfaf435c0911a6524b4251028d77
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2017-08-21 01:02:33 -0300

    Connection Class

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$db = new \Source\Conn('mysql:host=localhost;dbname=test_oo', 'root', 'root');
---
+++ src/Source/IConn.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Interface responsável por fazer conexão com o banco de Dados
+ * @author Matheus Flauzino
+ * @version 0.0.1
+ *
+ **/
+namespace Source;
+
+interface IConn {
+	public function connect();
+}
